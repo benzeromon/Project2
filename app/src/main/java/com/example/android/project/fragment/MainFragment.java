2f1be2da4d0fdacfd99f9f6cd359b09344b6750a
kamolratsomchai08 <kmlrtsomchai@yahoo.com>
2018-04-26 15:46:34 +0700

    26 April 61

--- app/src/main/java/com/example/android/project/fragment/MainFragment.java
+++ app/src/main/java/com/example/android/project/fragment/MainFragment.java
@@ -4,12 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.project.R;
+import com.example.android.project.utility.GetAllData;
+import com.example.android.project.utility.MyAlert;
+import com.example.android.project.utility.MyConstant;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class MainFragment extends Fragment{
 
@@ -17,24 +28,106 @@ public class MainFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // Register controller
-        TextView textView = getView().findViewById(R.id.txtRegister);
-        textView.setOnClickListener(new View.OnClickListener() {
+//        Register Controller
+        registerController();
+
+//        Login Controller
+        loginController();
+
+
+    }   // Main Method
+
+    private void loginController() {
+
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Replace Fragment
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.contentMainFragment, new RegisterFragment())
-                        .addToBackStack(null)
-                        .commit();
+
+                EditText userEditText = getView().findViewById(R.id.editUser);
+                EditText passwordEditText = getView().findViewById(R.id.editPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.normalDialog("Have Space",
+                            "Please Fill Every Blank");
+
+                } else {
+//                    No Space
+
+                    MyConstant myConstant = new MyConstant();
+                    boolean b = true;
+                    String truePass = null, nameUser = null;
+                    MyAlert myAlert = new MyAlert(getActivity());
+
+                    try {
+
+                        GetAllData getAllData = new GetAllData(getActivity());
+                        getAllData.execute(myConstant.getUrlGetAllUser());
+
+                        String jsonString = getAllData.get();
+                        Log.d("26AprilV1", "JSON ==> " + jsonString);
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+                        for (int i=0; i<jsonArray.length(); i+=1) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString("User"))) {
+                                b = false;
+                                truePass = jsonObject.getString("Password");
+                                nameUser = jsonObject.getString("Name");
+                            }
+                        }
+
+                        if (b) {
+                            myAlert.normalDialog("User False",
+                                    "No User in my DataBase");
+
+                        } else if (passwordString.equals(truePass)) {
+                            Toast.makeText(getActivity(), "Welcome"+nameUser,
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            myAlert.normalDialog("Password False",
+                                    "Please Try Agains Password False");
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+
+                    }
+
+
+
+
+                }   // if
 
 
 
             }
         });
 
+    }
 
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
     }
 
     @Nullable
@@ -43,5 +136,4 @@ public class MainFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
     }
-
-} // Main Class
+}   // Main Class
--- app/src/main/java/com/example/android/project/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/android/project/fragment/RegisterFragment.java
@@ -5,13 +5,22 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.project.MainActivity;
 import com.example.android.project.R;
+import com.example.android.project.utility.AddNewUserToServer;
+import com.example.android.project.utility.MyAlert;
+import com.example.android.project.utility.MyConstant;
 
 public class RegisterFragment extends Fragment {
 
@@ -23,9 +32,76 @@ public class RegisterFragment extends Fragment {
 
     }//Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+            uploadValueToServer();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToServer() {
+
+//        get_value_from EditText
+        EditText nameEditText = getView().findViewById(R.id.editName);
+        EditText useEditText = getView().findViewById(R.id.editUser);
+        EditText passwordEditText = getView().findViewById(R.id.editPassword);
+
+//        Change Data Type from EditText to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = useEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+
+//       Check Space
+        if (nameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) {
+//            Have Space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space", "Please Fill all Blank");
+
+        } else {
+//            No Space
+            try {
+                MyConstant myConstant = new MyConstant();
+                AddNewUserToServer addNewUserToServer = new AddNewUserToServer(getActivity());
+                addNewUserToServer.execute(nameString, userString, passwordString,
+                        myConstant.getUrlAddUser());
+                String result = addNewUserToServer.get();
+                Log.d("26AprilV1","result==>"+result);
+
+                if (Boolean.parseBoolean(result)){
+                    getActivity().getSupportFragmentManager().popBackStack();
+
+                }else {
+                    Toast.makeText(getActivity(),"Error Cannot Upload",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
     private void createToolbar() {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
         // Setup Title
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -41,6 +117,8 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
     }
 
     @Nullable
